01d553eeb7b1ae8bae8924867e2ebbc1df0b311e
James Azam <james.m.azam@gmail.com>
2024-03-29 21:30:32 +0000

    Move to using markdown for long descriptions

--- R/app.R
+++ R/app.R
@@ -20,25 +20,21 @@ epiparameter_data <- epiparameter_data
 ui <- shiny::fluidPage(
     theme = shinythemes::shinytheme("darkly"),
     # App title ----
-    shiny::titlePanel("A Dashboard for Visualising Published Epidemiological
-                      Parameters"
+    shiny::titlePanel(
+      shiny::markdown(
+        "
+        A Dashboard for Visualising Published Epidemiological Parameters
+        ---
+        "
+      )
     ),
-    shiny::markdown(
-      "This dashboard provides a quick way to visualise the epidemiological
-      parameters stored in the
-      [epiparameter R package](epiverse-trace/epiparameter).
-
-      The dashboard is intended to reduce the barrier of entry for those
-      interested in exploring epidemiological parameters. It is designed to
-      be user-friendly and interactive, allowing users to select a disease
-      and an epidemiological parameter to visualise. If the user is satisfied
-      with the visualised parameter, they can copy the citation for the
-      parameter directly from the \"citation\" field of the dashboard.
-
-      This dashboard is a proof of concept. If you have any feedback, please
-      feel free to open an issue or discussion on the package's
-      [GitHub repository](github.com/jamesmbaazam/epiparameterDashboard)."
-    ),
+    #nolint start: line_length_linter
+    # Using html tools opens up the possibility of including markdown files
+    # directly in the app. This is useful for long descriptions.
+    # shiny::markdown() is limited to the kinds of markdown that can be
+    # rendered in a single string.
+    htmltools::includeMarkdown("descriptions_md/dashboard_motivation.md"),
+    #nolint end
     # Sidebar panel for inputs ----
     shiny::sidebarLayout(
       # Input: Slider for the number of bins ----
---
+++ descriptions_md/dashboard_motivation.md
@@ -0,0 +1,11 @@
+This dashboard provides a quick way to visualise the epidemiological parameters
+stored in the [epiparameter R package](epiverse-trace/epiparameter)).
+
+The dashboard is intended to reduce the barrier of entry for those interested in
+exploring epidemiological parameters. <br/>It is designed to be user-friendly and interactive, allowing users to select a disease and an epidemiological parameter <br/>to visualise.
+
+If the user is satisfied with the visualised parameter, they can copy the
+citation for the parameter directly from the "citation" <br/> field of the dashboard.
+
+This dashboard is a proof of concept. If you have any feedback, please feel free
+to open an issue or discussion on the <br/> package's [GitHub repository](github.com/jamesmbaazam/epiparameterDashboard).
